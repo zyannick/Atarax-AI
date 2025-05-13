c6c826648c861cdc7fc0551442aeb5d8f90b820a
Yannick Zoetgnande <yannick.zoetgnande@bioaster.org>
2025-05-13 15:03:46 +0200

    start setuping llama.cpp

---
+++ setup_llama.cpp.sh
@@ -0,0 +1,14 @@
+git clone https://github.com/ggml-org/llama.cpp.git
+cd llama.cpp
+rm -rf build CMakeCache.txt CMakeFiles
+conda install conda-forge::curl
+conda install -n ataraxai libcurl libssh openldap
+conda install libcurl libidn2 libpsl libssh openldap
+mkdir build
+cd build
+cmake .. \
+  -DCMAKE_PREFIX_PATH=$CONDA_ENV_PATH \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DCURL_LIBRARY=$CONDA_ENV_PATH/lib/libcurl.so \
+  -DCURL_INCLUDE_DIR=$CONDA_ENV_PATH/include
+cd ../..
